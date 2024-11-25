1b76b53e8ed864a8c2902fa95fead293cedc4b44
rishitiwary <78086033+rishitiwary@users.noreply.github.com>
2024-11-25 11:14:01 +0530

    Update baseUrl.ts

--- src/config/baseUrl.ts
+++ src/config/baseUrl.ts
@@ -1,4 +1,4 @@
-export const baseUrl='https://testaddpi.grozep.com';
-export const mediaUrl='https://testaddpi.grozep.com';
+export const baseUrl='https://api.grozep.com';
+export const mediaUrl='https://api.grozep.com';
    
  
